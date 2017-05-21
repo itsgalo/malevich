bc729775bc7d56aed5c5eae7dc78ae6ecebfe0a4
José Cañizares <jose.canizares@colorado.edu>
2017-05-21 11:25:28 -0600

    switch from react-sketch to react-fabricjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Canvas from './SketchField';
+import Canvas from './Fabric';
 
 class App extends Component {
   render() {
