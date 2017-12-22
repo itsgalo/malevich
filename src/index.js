941c41f7a21977df870dd4ed8a901760aa5d71d4
Galo <galo.canizares@gmail.com>
2017-12-21 21:26:04 -0500

    new shapes, new buttons, new interactions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-//import main from './main'
-import main from './BlockTest'
+import main from './main'
 import "./scss/main.scss"
 
 export default (document) => {
--- src/main.js
+++ src/main.js
@@ -1,98 +1,266 @@
 import Matter from 'matter-js'
+import p5 from 'p5'
+import Dom from 'p5/lib/addons/p5.dom'
 
-// module aliases
-    var Engine = Matter.Engine,
-        Render = Matter.Render,
-        World = Matter.World,
-        Bodies = Matter.Bodies,
-        Events = Matter.Events,
-        MouseConstraint = Matter.MouseConstraint,
-        Mouse = Matter.Mouse,
-        Body = Matter.Body;
 
-    var root = document.getElementById('root');
-    var width = window.getComputedStyle(root).width;
-    var height = window.getComputedStyle(root).height;
+//window settings
+var w = window.innerWidth;
+var h = window.innerHeight;
+var canvas;
+var gravButton;
+var shapeButton;
 
-    var engine = Engine.create();
+//Drawing constants
+var painting = false;
+var currentPos;
+var previousPos;
+var shapes = [];
+var c = true;
+var s = false;
+var rectH;
+var rectW;
 
-  //   var engine = Engine.create({
-  //      element: document.body,
-  //      options: {
-  //        width: parseInt(width),
-  //        height: parseInt(height)
-  //    }
-  //  });
+//Matter constants
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Bodies = Matter.Bodies,
+  Body = Matter.Body,
+  Runner = Matter.Runner;
+//local Matter vars
+var engine;
+var world;
+var runner;
+//mouse interaction vars
+var Mouse = Matter.Mouse;
+var MouseConstraint = Matter.MouseConstraint;
+var mConstraint;
 
-    // create two boxes and a ground
-    var ball = Bodies.circle(420, 15, 20);
+//init sketch
+const sketch = (p5) => {
 
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+  //drawing method
+  function circ(x, y, w, h) {
 
-    // Add Mouse Controls
-    var mouse = Mouse.create();
-    var mouseConstraint = MouseConstraint.create(engine, {
-            mouse: mouse,
-            constraint: {
-                stiffness: 0.2,
-                render: {
-                      visible: false
-                  }
-            }
-        });
+    var options = {
+      friction : 0.9,
+      restitution : 0.6
+    }
+    this.body = Bodies.circle(x, y, p5.dist(x, y, w, h), options);
+    this.w = w;
+    this.h = h;
+    this.b = this.body.bounds;
 
-    // Add event handler for mouse events
-    /* NOTE: Using startdrag and enddrag so we can have a reference to the body
-       in the physics model that is being click by the mouse.*/
-    Events.on(mouseConstraint, 'startdrag', mouseDown);
-    Events.on(mouseConstraint, 'enddrag', mouseUp);
+    World.add(world, this.body);
 
-    function mouseDown() {
-      console.log('mouse down');
+    this.isOffCanvas = function(){
+      var pos = this.body.position;
+      return (pos.y < 0);
     }
 
-    function mouseUp() {
-      console.log('mouse up');
+    this.forget = function(){
+      World.remove(world, this.body);
     }
 
-    // create a renderer
-    var render = Render.create({
-        element: document.getElementById('root'),
-        engine: engine
-    });
+    this.show = function() {
+      var pos = this.body.position;
 
-    World.add(engine.world, mouseConstraint);
+      p5.fill(40);
+      p5.stroke(40);
+      p5.push();
+      p5.translate(pos.x, pos.y)
 
-    // keep the mouse in sync with rendering
-    render.mouse = mouse;
+      p5.ellipse(0, 0,p5.dist(x, y, this.w, this.h));
+      p5.pop();
+    }
+  }
 
-    window.addEventListener('resize', resize_canvas)
+  function square(x, y, w, h) {
 
-    resize_canvas()
+    var options = {
+      friction : 0.9,
+      restitution : 0.6,
+      angle: 0
+    }
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.w = w;
+    this.h = h;
+    this.b = this.body.bounds;
 
-    function resize_canvas() {
-      var canvas = document.getElementsByTagName('canvas')
-      console.log(canvas[0]);
-      var w = window.innerWidth;
-      var h = window.innerHeight;
-      canvas[0].setAttribute('width',w)
-      canvas[0].setAttribute('height',h)
-      canvas[0].setAttribute('style','object-fit:contain;')
+    World.add(world, this.body);
+
+    this.isOffCanvas = function(){
+      var pos = this.body.position;
+      return (pos.y < 0);
     }
 
-    // fit the render viewport to the scene
-    // Render.lookAt(render, {
-    //     min: { x: 0, y: 0 },
-    //     max: { x: width, y: height }
-    // });
+    this.forget = function(){
+      World.remove(world, this.body);
+    }
 
-    // add all of the bodies to the world
-    World.add(engine.world, [ball, boxA, boxB, ground]);
+    this.show = function() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
 
-    // run the engine
-    Engine.run(engine);
+      p5.fill(40)
+      p5.stroke(40);
+      p5.push();
+      p5.rectMode(p5.CENTER);
+      p5.translate(pos.x, pos.y);
+      p5.rotate(angle);
+      p5.rect(0, 0, this.w, this.h);
+      p5.pop();
 
-    // run the renderer
-    Render.run(render);
+    }
+  }
+
+
+function toggleGravity() {
+
+  if(runner.enabled == false){
+    gravButton.style("background-color", p5.color(255, 50, 50));
+    runner.enabled = true;
+    Runner.start(runner, engine);
+  } else if (runner.enabled == true) {
+    gravButton.style("background-color", p5.color(250, 130, 40));
+    Runner.stop(runner)
+    runner.enabled = false;
+  }
+
+}
+
+function toggleShape() {
+  if (c == true) {
+    shapeButton.style("background-color", p5.color(255, 50, 50));
+    s = true;
+    c = false;
+  } else if (c == false) {
+    shapeButton.style("background-color", p5.color(250, 130, 40));
+    c = true;
+    s = false;
+  }
+}
+
+p5.setup = function(){
+  canvas = p5.createCanvas(w, h);
+  p5.background(0);
+  p5.ellipseMode(p5.RADIUS);
+
+  var titleButton = p5.createButton('Malevi.ch');
+  titleButton.position(20, 20);
+  //gravity button
+  gravButton = p5.createButton('GRAVITY');
+  gravButton.position(w/2 - 170, 20);
+  gravButton.mousePressed(toggleGravity);
+
+  //shapes button
+  shapeButton = p5.createButton('SHAPE');
+  shapeButton.position(w/2 + 30, 20);
+  shapeButton.mousePressed(toggleShape);
+
+  currentPos = p5.createVector(0, 0);
+  previousPos = p5.createVector(0, 0);
+
+  //Matter init
+  engine = Engine.create();
+  world = engine.world;
+  runner = Runner.create();
+  runner.enabled = false;
+  //mouse
+  var mouse = Mouse.create(canvas.elt);
+  var mousePar = {
+    mouse: mouse,
+    constraint: {
+      stiffness: 0.9,
+    }
+  }
+  mConstraint = MouseConstraint.create(engine, mousePar);
+  mConstraint.mouse.pixelRatio = p5.pixelDensity();
+  World.add(world, mConstraint);
+
+  //boundaries
+  var params = {
+    isStatic: true,
+    friction: 0.5
+  }
+  var ground = Bodies.rectangle(w / 2, h, w, 10, params);
+  var wall1 = Bodies.rectangle(0, h / 2, 10, h, params);
+  var wall2 = Bodies.rectangle(w, h / 2, 10, h, params);
+  //var top = Bodies.rectangle(width / 2, 0, width, 1, params);
+  World.add(world, ground);
+  World.add(world, wall1);
+  World.add(world, wall2);
+}
+//p5 Draw
+p5.draw = function() {
+
+  if (painting == true){
+    p5.background(230, 50);
+  } else if (painting == false){
+    p5.background(230);
+  }
+  //Draw shape
+  //Grab mouse position
+  currentPos.x = p5.mouseX;
+  currentPos.y = p5.mouseY;
+
+  for (var i = 0; i < shapes.length; i++) {
+    shapes[i].show();
+    if (shapes[i].isOffCanvas()) {
+      shapes[i].forget();
+      shapes.splice(i, 1);
+    }
+  }
+
+}
+//stores initial click
+p5.mousePressed = function() {
+  painting = true;
+  p5.noStroke();
+  previousPos.x = p5.mouseX;
+  previousPos.y = p5.mouseY;
+
+}
+//bakes the shape
+p5.mouseReleased = function() {
+
+    painting = false;
+    if (mConstraint.body == null) {
+
+      if (c == true){
+        shapes.push(new circ(previousPos.x, previousPos.y, currentPos.x, currentPos.y));
+
+      } else if (s == true) {
+        shapes.push(new square(previousPos.x, previousPos.y, rectW, rectH));
+      }
+    }
+}
+//draws guide lines
+p5.mouseDragged = function() {
+  painting = true;
+  //rectangle geometry calc
+  var rh = p5.sq(p5.dist(previousPos.x, previousPos.y, currentPos.x, currentPos.y));
+  var rh2 = p5.sq(p5.dist(previousPos.x, 0, currentPos.x, 0));
+  rectH = p5.sqrt(rh-rh2);
+  var rw = p5.sq(p5.dist(previousPos.x, previousPos.y, currentPos.x, currentPos.y));
+  var rw2 = p5.sq(p5.dist(0, previousPos.y, 0, currentPos.y));
+  rectW = p5.sqrt(rw-rw2);
+
+  p5.fill(230, 200);
+  p5.stroke(40);
+    if (c == true) {
+      p5.ellipse(previousPos.x, previousPos.y, p5.dist(previousPos.x, previousPos.y, currentPos.x, currentPos.y));
+    } else if (s == true) {
+      p5.rectMode(p5.CENTER);
+      p5.rect(previousPos.x, previousPos.y, rectW, rectH);
+    }
+
+
+}
+
+p5.windowResized = function() {
+  p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
+}
+
+}
+
+new p5(sketch);
